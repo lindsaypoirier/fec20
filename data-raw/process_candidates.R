a64c7d2d5d8728ce7a9d435f4909b9e1d175102e
Ben Baumer <ben.baumer@gmail.com>
2020-02-26 12:41:10 -0500

    cleanup up candidates import; #1; #2

--- data-raw/process_candidates.R
+++ data-raw/process_candidates.R
@@ -1,36 +1,23 @@
 library(tidyverse)
-library(stringr)
 library(usethis)
 
 #reading files
-# candidates <- read.delim("~/Downloads/cn.txt", header = FALSE, sep = "|", quote="") %>%
-#   write_csv(path = "data-raw/candidates.csv")
 
-#data tidying
-candidates <- read_csv("data-raw/candidates.csv") %>%
-  rename(cand_id          = V1,
-         cand_name        = V2,
-         cand_pty_aff     = V3,
-         cand_election_yr = V4,
-         cand_office_st   = V5,
-         cand_office      = V6,
-         cand_office_dis  = V7,
-         cand_ici         = V8,
-         cand_status      = V9,
-         cand_pcc         = V10,
-         cand_sti         = V11,
-         cand_st2         = V12,
-         cand_city        = V13,
-         cand_st          = V14,
-         cand_zip         = V15
-  ) %>%
-  mutate(cand_pty_aff     = as.character(cand_pty_aff),
-         cand_election_yr = as.integer(cand_election_yr),
-         cand_office_st   = as.character(cand_office_st),
-         cand_office      = as.character(cand_office),
-         cand_office_dis  = as.integer(cand_office_dis),
-         cand_ici         = as.character(cand_ici),
-         cand_status      = as.character(cand_status),
-         cand_st          = as.character(cand_st))
+usethis::use_zip(
+  "https://www.fec.gov/files/bulk-downloads/2016/cn16.zip",
+  destdir = tempdir(), cleanup = TRUE
+)
+
+cand_path <- fs::path(tempdir(), "cn16", "cn.txt")
+
+cand_names <- read_csv("https://www.fec.gov/files/bulk-downloads/data_dictionaries/cn_header_file.csv") %>%
+  names() %>%
+  tolower()
+
+candidates <- read_delim(
+  cand_path,
+  col_names = cand_names,
+  delim = "|"
+)
 
 usethis::use_data(candidates, overwrite = TRUE)
