617663b2e65e9358fd77b282fa082b55ea588bff
Ben Baumer <ben.baumer@gmail.com>
2020-02-27 09:41:48 -0500

    fixed up contributions; close #5

---
+++ data-raw/process_contributions.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+dir <- usethis::use_zip(
+  "https://www.fec.gov/files/bulk-downloads/2016/pas216.zip",
+  destdir = tempdir(), cleanup = TRUE
+)
+
+file_path <- fs::path(dir, "itpas2.txt")
+
+exp_names <- read_csv("https://www.fec.gov/files/bulk-downloads/data_dictionaries/pas2_header_file.csv") %>%
+  names() %>%
+  tolower()
+
+contributions <- read_delim(
+  file_path,
+  col_names = exp_names,
+  col_types = cols(
+    employer = col_character(),
+    occupation = col_character()
+  ),
+  delim = "|"
+) %>%
+  select(-employer, -occupation, -memo_cd, -memo_text, -sub_id)
+#  filter(primary_general_indicator == "G2016") %>%
+#  group_by(cand_id) %>%
+#  summarise(number_of_contributions = n(), total_contributions = sum(transaction_amount[transaction_amount>0]), net_contributions = sum(transaction_amount))
+
+usethis::use_data(committee_contributions, overwrite = TRUE)
