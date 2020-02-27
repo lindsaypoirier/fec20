232b1d27b85312ab668b647d14002a18398e0286
Ben Baumer <ben.baumer@gmail.com>
2020-02-27 08:53:32 -0500

    updated house_results; close #3

--- data-raw/process_results.R
+++ data-raw/process_results.R
@@ -1,32 +1,27 @@
 library(tidyverse)
-library(stringr)
-library(usethis)
-library(janitor)
-library(purrr)
 
-# read data and tame dat
-# results <- read_csv("data-raw/results16.csv") %>%
-#   clean_names() %>%
-#   # delete unneccesary variables
-#   select(-state, -total_votes, -total_votes_number) %>%
-#   rename( state = state_abbreviation,
-#           cand_id = fec_id ) %>%
-#   mutate(state                 = as.character(state),
-#          winner_indicator      = as.character(winner_indicator),
-#          general_election_date = as.Date(general_election_date, "%m/%d/%y"),
-#          party                 = as.character(party),
-#          #remove percent sign
-#          general_percent       = map(general_percent, ~gsub("%", "", .x)),
-#          general_percent       = as.numeric(general_percent)
-#          )
+downloader::download(
+  "https://transition.fec.gov/pubrec/fe2016/federalelections2016_000.xlsx",
+  destfile = fs::path(tempdir(), "results16.xlsx")
+)
 
-# change NA to FALSE
-results <- results %>% replace_na(list(winner_indicator = FALSE))
+house_results <- readxl::read_excel(fs::path(tempdir(), "results16.xlsx"), sheet = 13) %>%
+   janitor::clean_names() %>%
+   # delete unneccesary variables
+   select(-x1, -state, -total_votes, -contains("combined")) %>%
+   rename(
+     state = state_abbreviation,
+     cand_id = fec_id_number,
+     distict_id = d,
+     incumbent = i,
+     won = ge_winner_indicator
+   ) %>%
+  filter(cand_id != 'n/a', !str_detect(cand_id, "FULL TERM")) %>%
+  mutate(
+    primary_votes = parse_number(primary_votes),
+    general_votes = parse_number(general_votes),
+    won = won == "W"
+  ) %>%
+  replace_na(list(won = FALSE))
 
-# change W to TRUE
-results$winner_indicator[results$winner_indicator == "W"] <- TRUE
-
-# change from character to logical
-results <- results %>% mutate(winner_indicator = as.logical(winner_indicator))
-
-usethis::use_data(results, overwrite = TRUE)
+usethis::use_data(house_results, overwrite = TRUE)
